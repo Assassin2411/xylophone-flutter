ad8ca70af503b10bef4ced38578550a92544a669
Amit Choudhary <79579172+Assassin2411@users.noreply.github.com>
2023-04-12 22:34:42 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber){
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.mp3'));
+  }
+
+  Expanded buildKey(Color color, int number){
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: color),
+        onPressed: () {
+          playSound(number);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
